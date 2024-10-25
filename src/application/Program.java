d702bcd00908b9b320ff1957ccbd83cb6e3c7d3b
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-10-25 00:50:35 -0300

    Interface Consumer implementation

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
+import utils.UpdatePrice;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<Product> products = new ArrayList<>();
+		
+		products.add(new Product("Macbook Pro", 25000.0));
+		products.add(new Product("iPhone 16", 16000.0));
+		products.add(new Product("Notebook Lenovo", 5000.00));
+		products.add(new Product("Airpods", 200.00));
+		products.add(new Product("Smart TV LG 4K", 4850.00));
+		products.add(new Product("External SSD 1Tb", 785.00));
+		
+		products.forEach(new UpdatePrice());
+		
+		products.forEach(System.out::println);
+	}
+}
